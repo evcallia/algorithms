94d237615463dbca5197c764eaf6e0748de60799
Evan <calliaevan12@gmail.com>
2017-01-30 18:03:46 -0800

    array- parent children complete

--- arrays.js
+++ arrays.js
@@ -41,6 +41,56 @@ function RemoveNegatives(arr){
 // console.log(myArr);
 
 
+/*
+Suppose we have some input data describing relationships between parents and children. The data is formatted as a list of (parent, child) pairs, where each individual is assigned a unique integer identifier.
+parentChildPairs =
+    [[1, 3], [2, 3], [3, 6], [5, 6], [5, 7],[4, 5], [4, 8], [8, 9]]
+For example, in this diagram, 3 is a child of 1 and 2, and 5 is a child of 4:
+
+ 1   2     4
+  \ /     / \
+   3     5   8
+    \   / \   \
+     \ /   \   9
+      6     7
+Write a function that takes this data as input and outputs two collections: one containing all individuals with zero known parents, and one containing all individuals with exactly one known parent.
+Sample output:
+Zero parents: 1, 2, 4
+One parent: 5, 7, 8, 9
+Below is some sample data in JavaScript. Feel free to solve this problem in any language of your choice.
+var parentChildPairs =
+    [[1, 3], [2, 3], [3, 6], [5, 6], [5, 7], [4, 5], [4, 8], [8, 9]];
+*/
+
+function parentChildren(arr){
+    var dict = {};
+    for(var i = 0; i < arr.length; i++){
+        if(arr[i][0] in dict){
+            // do nothing
+        }else{
+            dict[arr[i][0]] = 0;
+        }
+        if(arr[i][1] in dict){
+            dict[arr[i][1]] += 1;
+        }else{
+            dict[arr[i][1]] = 1;
+        }
+    }
+    var parent0 = [];
+    var parent1 = [];
+    for(key in dict){
+        if(dict[key] == 0){
+            parent0.push(key);
+        }
+        if(dict[key] == 1){
+            parent1.push(key);
+        }
+    }
+    return [parent0, parent1];
+}
+
+var test = [[1, 3], [2, 3], [3, 6], [5, 6], [5, 7],[4, 5], [4, 8], [8, 9]]
+console.log(parentChildren(test));
 
 
 
